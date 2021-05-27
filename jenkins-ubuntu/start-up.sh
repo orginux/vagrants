d6627f15a904117fe672820de1d5f13bd351a436
orginux <orginux@protonmail.com>
2021-05-27 22:19:26 +0300

    install java and check versions

--- jenkins-ubuntu/start-up.sh
+++ jenkins-ubuntu/start-up.sh
@@ -1,6 +1,12 @@
 sudo apt-get update && sudo apt-get upgrade -y
+sudo apt install -y openjdk-11-jdk
+
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
+
 sudo apt-get autoremove && sudo apt-get clean
+
+java -version
+sudo apt list --installed | grep jenkins
