e56a15b2b1f484c2dfbdb35e3997813b911ed6ae
orginux <orginux@protonmail.com>
2021-05-27 22:46:48 +0300

    update script

--- jenkins-ubuntu/start-up.sh
+++ jenkins-ubuntu/start-up.sh
@@ -1,12 +1,31 @@
 sudo apt-get update && sudo apt-get upgrade -y
-sudo apt install -y openjdk-11-jdk
 
+# java
+sudo apt-get install -y openjdk-11-jdk
+
+# jenkins
 wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
 
+# docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# clean cache
 sudo apt-get autoremove && sudo apt-get clean
 
+# versions
 java -version
-sudo apt list --installed | grep jenkins
+sudo apt-get list --installed | grep jenkins
